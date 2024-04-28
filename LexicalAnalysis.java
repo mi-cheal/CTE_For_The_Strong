47523ff52360bf54679c73f3fb524d7a17eca3b4
mi-cheal <116207443+mi-cheal@users.noreply.github.com>
2024-04-28 13:21:54 +0200

    Update and rename LexicalAnalyzer.java to LexicalAnalysis.java

--- LexicalAnalysis.java
+++ LexicalAnalysis.java
@@ -2,29 +2,25 @@ import java.util.ArrayList;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-public class LexicalAnalyzer {
-    public static void main(String[] args) {
-        String input = "INTEGER M, N, K, P, R, H, |, g, k, m";
+public class LexicalAnalysis {
+    public static ArrayList<String> tokenize(String program) {
         ArrayList<String> tokens = new ArrayList<>();
 
         // Define regular expressions for different token types
-        String keywordRegex = "\\b(INTEGER|ASSIGN|READ|WRITE|START|STOP)\\b";
+        String keywordRegex = "\\b(INTEGER|ASSIN|READ|WRITE|START|STOP)\\b";
         String identifierRegex = "\\b[a-zA-Z]\\w*\\b";
         String symbolRegex = "[,|]";
         String errorRegex = "[^\\s]"; // Any character not allowed
 
         String regex = String.format("(%s)|(%s)|(%s)|(%s)", keywordRegex, identifierRegex, symbolRegex, errorRegex);
         Pattern pattern = Pattern.compile(regex);
-        Matcher matcher = pattern.matcher(input);
+        Matcher matcher = pattern.matcher(program);
 
         while (matcher.find()) {
             String token = matcher.group();
             tokens.add(token);
         }
 
-        // Output tokens
-        for (String token : tokens) {
-            System.out.println(token);
-        }
+        return tokens;
     }
 }
